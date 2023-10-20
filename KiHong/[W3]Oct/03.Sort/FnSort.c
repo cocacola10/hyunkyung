98e4fe6182b8f85ab57af39adc42f92c2bf1ea8c
root <root@DESKTOP-UHP87QS>
2023-10-20 10:04:29 +0900

    ♻ refactor : FnSort.c script refactoring

---
+++ KiHong/[W3]Oct/03.Sort/FnSort.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+
+void BubbleSort(int array[], int size);
+void PrintOutArray(int array[], int size);
+
+int main(void) {
+    int array[6] = { 7, 5, 1, 4, 3, 6 };
+    int size = sizeof(array)/sizeof(array[0]);
+
+    BubbleSort(array, size);
+    PrintOutArray(array, size);
+    
+    return 0;
+}
+
+void BubbleSort(int array[], int size) {
+    for (int k = 0; k < size; k++) {
+        for (int j = k + 1; j < size; j++) {
+            if (array[k] > array[j]) {
+                int temp = array[k];
+                array[k] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+}
+
+void PrintOutArray(int array[], int size) {
+    for (int k = 0; k < size; k++) {
+        printf("%d ", array[k]);
+    }
+    printf("\n");
+}
